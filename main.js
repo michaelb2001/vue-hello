67a0f7668d90ea5c10385410c3b144a1d7797dcc
michaelb2001 <brunco.michael@gmail.com>
2022-01-13 13:21:03 +0100

    prima versione con vue.js

---
+++ main.js
@@ -0,0 +1,9 @@
+var myObject = new Vue({
+    el: '#root',
+    data: {
+        message: "hello world",
+        photo: "https://archzine.it/wp-content/uploads/2020/01/sfondo-scuro-telefono-cellulare-immagine-scaricare-fumo-colore-rosso.jpg",
+
+        miaclasse : "text-white text-center"
+    }
+});
